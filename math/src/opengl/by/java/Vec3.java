3961c64e11f23ba514135fffd70b40f2ffcde76f
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-10-14 17:44:43 +0900

    add: cross method and dot method in Vec3

--- math/src/opengl/by/java/Vec3.java
+++ math/src/opengl/by/java/Vec3.java
@@ -3,11 +3,27 @@ package opengl.by.java;
 /** Vector **/
 public class Vec3 {
 	public final float x, y, z;
+	public final Vec3 ZERO = new Vec3(0, 0, 0);
 
 	public Vec3(float x, float y, float z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+	}
+
+	public Vec3 cross(Vec3 target) {
+        float rx = Math.fma(y, target.z, -z * target.y);
+        float ry = Math.fma(z, target.x, -x * target.z);
+        float rz = Math.fma(x, target.y, -y * target.x);
+		return new Vec3(
+			Math.fma(y, target.z, -z * target.y),
+			Math.fma(z, target.x, -x * target.z),
+			Math.fma(x, target.y, -y * target.x)
+		);
+	}
+
+	public float dot(Vec3 target) {
+		return x * target.x + y * target.y + z * target.z;
 	}
 
 	public Vec3 plus(Vec3 target) {
@@ -27,7 +43,7 @@ public class Vec3 {
 	}
 
 	public Vec3 normalize() {
-		final var norm = (float)Math.sqrt(x + y + z);
+		final var norm = length();
 		if (norm == 1) return this;
 		if (norm == 0) throw new ArithmeticException("normalize by [0,0,0]");
 		return new Vec3(
@@ -35,6 +51,10 @@ public class Vec3 {
 			y / norm,
 			z / norm
 		);
+	}
+
+	public float length() {
+		return (float)Math.sqrt(x + y + z);
 	}
 
 	@Override
