0dbda32f644da920a0f74b7a0782f2a455458152
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-12-13 23:04:59 +0900

    add: some method to Vec3.scala

--- math/src/opengl/by/java/Vec3.scala
+++ math/src/opengl/by/java/Vec3.scala
@@ -1,4 +1,15 @@
 class Vec3(val x: Float, val y: Float, val z: Float) {
-    def +: (target: Vec3) = Vec3(x + target.x, y + target.y, z + target.z)
-    def -: (target: Vec3) = Vec3(x - target.x, y - target.y, z - target.z)
+    def +(target: Vec3) = Vec3(x + target.x, y + target.y, z + target.z)
+    def -(target: Vec3) = Vec3(x - target.x, y - target.y, z - target.z)
+    def *(f: Float)  = Vec3(x*f, y*f, z*f)
+    def *(target: Vec3) =
+        Vec3(
+            Math.fma(y, target.z, -z * target.y),
+            Math.fma(z, target.x, -x * target.z),
+            Math.fma(x, target.y, -y * target.x),
+        )
+    def dot(target: Vec3) = x*target.x + y*target.y + z*target.z
+    def normalize() = Vec3()
+    def length() = (x + y + z) / 3
+    def toString() = s"[$x, $y, $z]"
 }
