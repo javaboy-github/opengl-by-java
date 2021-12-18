13c0fe055daee20f6583a3744d54d948184b56b7
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-12-18 17:57:34 +0900

    fix

--- math/src/opengl/by/java/Mat4.scala
+++ math/src/opengl/by/java/Mat4.scala
@@ -1,7 +1,7 @@
 class Mat4(val m:Array[Float]) {
-    def +(target: Mat4) = new Mat4((m zip target.m).map((x, y) => x+y))
+    def +(target: Mat4) = new Mat4((m zip target.m).map(e => e._1 + e._2))
     def *(target: Mat4) =
-        new Mat4(
+        new Mat4(Array(
             get(1,1) * target.get(1,1) + get(1,2) * target.get(2,1) + get(1,3) * target.get(3,1) + get(1,4) * target.get(4,1),
             get(1,1) * target.get(1,2) + get(1,2) * target.get(2,2) + get(1,3) * target.get(3,2) + get(1,4) * target.get(4,2),
             get(1,1) * target.get(1,3) + get(1,2) * target.get(2,3) + get(1,3) * target.get(3,3) + get(1,4) * target.get(4,3),
@@ -18,10 +18,10 @@ class Mat4(val m:Array[Float]) {
             get(4,1) * target.get(1,2) + get(4,2) * target.get(2,2) + get(4,3) * target.get(3,2) + get(4,4) * target.get(4,2),
             get(4,1) * target.get(1,3) + get(4,2) * target.get(2,3) + get(4,3) * target.get(3,3) + get(4,4) * target.get(4,3),
             get(4,1) * target.get(1,4) + get(4,2) * target.get(2,4) + get(4,3) * target.get(3,4) + get(4,4) * target.get(4,4)
-        )
-    def get(row: Int, column: Int): Flaot = {
+        ))
+    def get(row: Int, column: Int): Float = {
         if (row > 4 || row < 1 || column > 4 || column < 1)
             throw new java.lang.IndexOutOfBoundsException()
-        return m[row-1 + (column-1)*4]
+        return m(row -1 + (column-1)*4)
     }
 }
